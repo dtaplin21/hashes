0e542d5318b6111a91b48e28eb7856711484da04
Dom <dtaplin21@aol.com>
2023-06-07 21:57:40 -0700

    hash

--- hash-table.js
+++ hash-table.js
@@ -11,23 +11,32 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here
+    this.data = new Array(bucketNums).fill(null);
+    this.capacity = 2;
+    this.count = 0// Your code here
   }
 
   hash(key) {
-    // Your code here
+    const hex = sha256(key).slice(0,8)
+    return parseInt(hex, 16)
   }
 
   hashMod(key) {
-    // Your code here
+    return this.hash(key) % this.capacity
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    const index = this.hashMod(key)
+    if(this.data[index]) {
+      throw new Error('hash collision or same key/value pair already exists!')
+    } else {
+      this.data[index] =
+    }
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+     const collide = this.insertNoCollisions()
+
   }
 
   insert(key, value) {
